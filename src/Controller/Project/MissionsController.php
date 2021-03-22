f4f790e300d04529543943ebea3394ddb11ff4fb
KiDayz <tomwperso@gmail.com>
2021-03-22 15:22:43 +0100

    UI Change : RM are handled 1 by 1

--- src/Controller/Project/MissionsController.php
+++ src/Controller/Project/MissionsController.php
@@ -14,9 +14,10 @@ namespace App\Controller\Project;
 use App\Entity\Project\Etude;
 use App\Entity\Project\Mission;
 use App\Entity\Project\RepartitionJEH;
-use App\Form\Project\MissionsType;
+use App\Form\Project\MissionType;
 use App\Service\Project\EtudePermissionChecker;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,36 +33,46 @@ class MissionsController extends AbstractController
      *
      * @return RedirectResponse|Response
      */
-    public function modifier(Request $request, Etude $etude, EtudePermissionChecker $permChecker)
+    public function modifier(
+        Request $request, 
+        Mission $mission, 
+        EtudePermissionChecker $permChecker)
     {
         $em = $this->getDoctrine()->getManager();
+
+        $etude = $mission->getEtude();
 
         if ($permChecker->confidentielRefus($etude, $this->getUser())) {
             throw new AccessDeniedException('Cette étude est confidentielle');
         }
 
         /* Form handling */
-        $form = $this->createForm(MissionsType::class, $etude, ['etude' => $etude]);
+        $form = $this->createForm(
+            MissionType::class, 
+            $mission,
+            ['etude' => $etude]);
+
+        $deleteForm = $this->createDeleteForm($mission->getId());
+
         if ('POST' == $request->getMethod()) {
             $form->handleRequest($request);
 
             if ($form->isValid()) {
-                foreach ($form->get('missions') as $missionForm) {
-                    $m = $missionForm->getData();
-                    foreach ($missionForm->get('repartitionsJEH') as $repartitionForm) {
-                        $r = $repartitionForm->getData();
-                        /* @var RepartitionJEH $r */
-                        $r->setMission($m);
-                    }
-                    /* @var Mission $m */
-                    $m->setEtude($etude);
+
+                $m = $form->getData();
+                foreach ($form->get('repartitionsJEH') as $repartitionForm) {
+                    $r = $repartitionForm->getData();
+                    /* @var RepartitionJEH $r */
+                    $r->setMission($m);
                 }
+                /* @var Mission $m */
+                $m->setEtude($etude);
 
                 $em->persist($etude);
                 $em->flush();
                 $this->addFlash('success', 'Mission enregistrée');
 
-                return $this->redirectToRoute('project_missions_modifier', ['id' => $etude->getId()]);
+                return $this->redirectToRoute('project_missions_modifier', ['id' => $mission->getId()]);
             }
             $this->addFlash('danger', 'Le formulaire contient des erreurs.');
         }
@@ -69,6 +80,101 @@ class MissionsController extends AbstractController
         return $this->render('Project/Mission/missions.html.twig', [
             'form' => $form->createView(),
             'etude' => $etude,
+            'delete_form' => $deleteForm->createView(),
         ]);
+    }
+
+    /**
+     * @Security("has_role('ROLE_SUIVEUR')")
+     * @Route(name="project_missions_ajouter", path="/suivi/missions/ajouter/{id}", methods={"GET","HEAD","POST"})
+     *
+     * @return RedirectResponse|Response
+     */
+    public function add(
+        Request $request,
+        Etude $etude,
+        EtudePermissionChecker $permChecker
+    ) {
+        $em = $this->getDoctrine()->getManager();
+
+        if ($permChecker->confidentielRefus($etude, $this->getUser())) {
+            throw new AccessDeniedException('Cette étude est confidentielle');
+        }
+
+        $mission = new Mission();
+        $etude->addMission($mission);
+
+        /* Form handling */
+        $form = $this->createForm(
+            MissionType::class, 
+            $mission,
+            ['etude' => $etude]);
+
+        $deleteForm = $this->createDeleteForm($mission->getId());
+
+        if ('POST' == $request->getMethod()) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+
+                $m = $form->getData();
+                foreach ($form->get('repartitionsJEH') as $repartitionForm) {
+                    $r = $repartitionForm->getData();
+                    /* @var RepartitionJEH $r */
+                    $r->setMission($m);
+                }
+                /* @var Mission $m */
+                $m->setEtude($etude);
+
+                $em->persist($etude);
+                $em->flush();
+                $this->addFlash('success', 'Mission enregistrée');
+
+                return $this->redirectToRoute('project_missions_modifier', ['id' => $mission->getId()]);
+            }
+            $this->addFlash('danger', 'Le formulaire contient des erreurs.');
+        }
+
+        return $this->render('Project/Mission/missions.html.twig', [
+            'form' => $form->createView(),
+            'etude' => $etude,
+            'delete_form' => $deleteForm->createView(),
+        ]);
+    }
+
+    /**
+     * @Security("has_role('ROLE_SUIVEUR')")
+     * @Route(name="project_mission_supprimer", path="/suivi/mission/supprimer/{id}", methods={"GET","HEAD","POST"})
+     *
+     * @return RedirectResponse
+     */
+    public function delete(Request $request, Mission $mission, EtudePermissionChecker $permChecker)
+    {
+        $form = $this->createDeleteForm($mission->getId());
+        $form->handleRequest($request);
+        $etude = $mission->getEtude();
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            if ($permChecker->confidentielRefus($etude, $this->getUser())) {
+                throw new AccessDeniedException('Cette étude est confidentielle');
+            }
+
+            $em->remove($mission);
+            $em->flush();
+            $this->addFlash('success', 'PV supprimé');
+        } else {
+            $this->addFlash('danger', 'Le formulaire contient des erreurs.');
+        }
+
+        return $this->redirectToRoute('project_etude_voir', ['nom' => $etude->getNom(), '_fragment' => 'tab3']);
+    }
+
+    private function createDeleteForm($id_mission)
+    {
+        return $this->createFormBuilder(['id' => $id_mission])
+            ->add('id', HiddenType::class)
+            ->getForm();
     }
 }
